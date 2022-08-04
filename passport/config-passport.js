b38fd8a27dca776946aef702ae3878af9147b4d7
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2022-08-04 13:35:29 +0300

    config

--- passport/config-passport.js
+++ passport/config-passport.js
@@ -1,27 +1,38 @@
 import passport from 'passport';
-import passportJWT from 'passport-jwt';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import User from '../models/user.js';
 import 'dotenv/config';
 import express from 'express';
 const SECRET_KEY = process.env.SECRET_KEY;
 
-const ExtractJWT = passportJWT.ExtractJwt;
-const Strategy = passportJWT.Strategy;
 const params = {
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 	secretOrKey: SECRET_KEY,
-	jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
 };
 
 // JWT Strategy
+// passport.use(
+// 	new Strategy(params, function (payload, done) {
+// 		User.find({ _id: payload.id })
+// 			.then(([user]) => {
+// 				if (!user) {
+// 					return done(new Error('User not found'), false);
+// 				}
+// 				return done(null, user);
+// 			})
+// 			.catch(err => done(err));
+// 	})
+// );
 passport.use(
-	new Strategy(params, function (payload, done) {
-		User.find({ _id: payload.id })
-			.then(([user]) => {
-				if (!user) {
-					return done(new Error('User not found'));
-				}
-				return done(null, user);
-			})
-			.catch(err => done(err));
+	new Strategy(params, async (payload, done) => {
+		try {
+			const user = await User.findById(payload.id);
+			if (!user) {
+				return done(new Error('User not found'), false);
+			}
+			return done(null, user);
+		} catch (error) {
+			return done(error, false);
+		}
 	})
 );
