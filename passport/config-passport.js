d59358c53bc2a3233bb1d8fbeaf0bd550926804e
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2022-08-07 18:57:12 +0300

    install-heroku

--- passport/config-passport.js
+++ passport/config-passport.js
@@ -23,3 +23,20 @@ passport.use(
 		}
 	})
 );
+
+// module.exports = passport => {
+// 	passport.use(
+// 		new JwtStrategy(opts, (jwt_payload, done) => {
+// 			User.findById(jwt_payload.id)
+// 				.then(user => {
+// 					if (user) {
+// 						return done(null, user);
+// 					}
+// 					return done(null, false);
+// 				})
+// 				.catch(err => {
+// 					console.log(err);
+// 				});
+// 		})
+// 	);
+// };
