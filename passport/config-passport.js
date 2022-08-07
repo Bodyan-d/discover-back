9dbf537bd229f458776687e3f8bc1f226c618648
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2022-08-07 19:07:18 +0300

    орм

--- passport/config-passport.js
+++ passport/config-passport.js
@@ -23,20 +23,3 @@ passport.use(
 		}
 	})
 );
-
-// module.exports = passport => {
-// 	passport.use(
-// 		new JwtStrategy(opts, (jwt_payload, done) => {
-// 			User.findById(jwt_payload.id)
-// 				.then(user => {
-// 					if (user) {
-// 						return done(null, user);
-// 					}
-// 					return done(null, false);
-// 				})
-// 				.catch(err => {
-// 					console.log(err);
-// 				});
-// 		})
-// 	);
-// };
