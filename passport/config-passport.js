0c7aa5938aeb4fa9efd12f2ea2f1b21d5a035db2
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2022-08-17 16:56:58 +0300

    delete-comment

--- passport/config-passport.js
+++ passport/config-passport.js
@@ -26,33 +26,3 @@ passport.use(
 		});
 	})
 );
-
-// import passport from 'passport';
-// import { ExtractJwt } from 'passport-jwt';
-// import { Strategy } from 'passport-local';
-// import User from '../models/user.js';
-// import dotenv from 'dotenv';
-// dotenv.config();
-// const SECRET_KEY = process.env.JWT_SECRET_KEY;
-
-// const params = {
-// 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-// 	secretOrKey: SECRET_KEY,
-// };
-
-// passport.use(
-// 	new Strategy(params, async (payload, done) => {
-// 		try {
-// 			const user = await User.findById(payload.id);
-// 			if (!user) {
-// 				return done(new Error('User not found'), false);
-// 			}
-// 			if (!user.token) {
-// 				return done(null, false);
-// 			}
-// 			return done(null, user);
-// 		} catch (error) {
-// 			return done(error, false);
-// 		}
-// 	})
-// );
